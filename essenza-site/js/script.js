e8cfd96ab579122d7b4a472f20ac0bd6f73083bb
Edson Junior <edson.diascoelho@gmail.com>
2024-05-21 13:02:29 -0300

    Atualização
    Seção de serviços

--- essenza-site/js/script.js
+++ essenza-site/js/script.js
@@ -1,44 +1,25 @@
 var swiper = new Swiper(".swiper", {
-  slidesPerView: 4.5,
-  // spaceBetween: 20,
+  slidesPerView: 5,
   loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   keyboard: true,
-  
-  breakpoints: {
-    // 640: {
-    //   slidesPerView: 2,
-    //   spaceBetween: 20,
-    // },
 
+  breakpoints: {
     300: {
-      // slidesPerView:2,
-      // spaceBetween:10,
-      // padding:16,
-      // centeredSlides: true,
+      slidesPerView: 2,
     },
-  
-    550: {
+    749: {
       slidesPerView: 3,
-      spaceBetween: 1,
-      centeredSlides: false,
     },
 
     1068: {
-      slidesPerView: 3,
-      // spaceBetween: 50,
+      slidesPerView: 4,
     },
-   
-    // 1200: {
-    // slidesPerView: 5,
-    //   spaceBetween: 3,
-    // },
-
-   
+    1300: {
+      slidesPerView: 5,
+    },
   },
 });
-
-
