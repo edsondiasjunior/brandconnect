7e954225fba3095911031d6f047f69233ff70db4
Edson Junior <edson.diascoelho@gmail.com>
2024-05-30 23:37:05 -0300

    Atualizaçao

--- essenza-site/js/script.js
+++ essenza-site/js/script.js
@@ -9,8 +9,33 @@ var swiper = new Swiper(".swiper", {
   keyboard: true,
 
   breakpoints: {
-    300: {
-      slidesPerView: 4,
+    1524: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+// CARROSSEL TESTE
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  navText: [
+    "<i class='fa fa-caret-left'></i>",
+    "<i class='fa fa-caret-right'></i>",
+  ],
+  autoplay: true,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
     },
   },
 });
