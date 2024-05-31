11a034d5f0747dd60f7a8b732c41f8c48a6f174a
Edson Junior <edson.diascoelho@gmail.com>
2024-05-31 08:39:39 -0300

    Atualização

--- essenza-site/js/script.js
+++ essenza-site/js/script.js
@@ -9,8 +9,21 @@ var swiper = new Swiper(".swiper", {
   keyboard: true,
 
   breakpoints: {
-    1524: {
-      slidesPerView: 5,
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    "@1.50": {
+      slidesPerView: 4,
+      spaceBetween: 50,
     },
   },
 });
